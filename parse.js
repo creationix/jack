d6bde80214496d5cae7792c5635189ce91f1c100
Tim Caswell <tim@creationix.com>
2013-01-10 20:55:25 -0600

    Fix the grammar to be LALR safe

--- parse.js
+++ parse.js
@@ -44,7 +44,7 @@ var grammar = {
   },
 
   operators: [
-    ["nonassoc", ':='],
+    ["nonassoc", ':=', '|', 'RETURN', 'LOOP'],
     ["right", '='],
     ["left", '||', '^^'],
     ["left", '&&'],
@@ -57,48 +57,72 @@ var grammar = {
 
   bnf: {
     root: [
-      ["", "return []"],
-      ["body", "return $1"],
+      ["block", "return $1"],
     ],
-    body: [
-      ["expr", "$$ = [$1]"],
-      ["body TERMINATOR expr", "$$ = $1.concat([$3])"],
-      ["body TERMINATOR", "$$ = $1"],
+    block: [
+      ["", "$$ = []"],
+      ["block line", "$$ = $1.concat([$2])"],
     ],
-    expr: [
+    multiBlock: [
+      ["line line", "$$ = [$1, $2]"],
+      ["multiBlock line", "$$ = $1.concat([$2])"],
+    ],
+    // One or more terminators
+    term: ["TERMINATOR", "term TERMINATOR"],
+    line: [
+      ["expr term", "$$ = $1"],
+      ["statement term", "$$ = $1"],
+    ],
+    params: [
+      ["IDENT", "$$ = [$1]"],
+      ["params IDENT", "$$ = $1.concat([$2])"],
+    ],
+    basic: [
+      ["{ block }", "$$ = ['FUNCTION', [], $2]"],
+      ["{ | params | block }", "$$ = ['FUNCTION', $3, $5]"],
+      ["IDENT", "$$ = ['IDENT', $1]"],
       ["( expr )", "$$ = $2"],
+      ["( multiBlock )", "$$ = ['BLOCK', $2]"],
       ["INTEGER", "$$ = ['VALUE', parseInt($1, 10)];"],
       ["CONSTANT", "$$ = ['VALUE', $1 === 'true' ? true : $1 === 'false' ? false : null];"],
       ["STRING", "$$ = ['VALUE', eval($1)];"],
-      ["expr + expr", "$$ = ['ADD', $1, $3];"],
-      ["expr - expr", "$$ = ['SUB', $1, $3];"],
-      ["expr * expr", "$$ = ['MUL', $1, $3];"],
-      ["expr / expr", "$$ = ['DIV', $1, $3];"],
-      ["expr < expr", "$$ = ['LT', $1, $3];"],
-      ["expr <= expr", "$$ = ['LTE', $1, $3];"],
-      ["expr > expr", "$$ = ['GT', $1, $3];"],
-      ["expr >= expr", "$$ = ['GTE', $1, $3];"],
-      ["expr == expr", "$$ = ['EQ', $1, $3];"],
-      ["expr ~= expr", "$$ = ['NEQ', $1, $3];"],
-      ["expr && expr", "$$ = ['AND', $1, $3];"],
-      ["expr || expr", "$$ = ['OR', $1, $3];"],
-      ["expr ^^ expr", "$$ = ['XOR', $1, $3];"],
+      ["basic + basic", "$$ = ['ADD', $1, $3];"],
+      ["basic - basic", "$$ = ['SUB', $1, $3];"],
+      ["basic * basic", "$$ = ['MUL', $1, $3];"],
+      ["basic / basic", "$$ = ['DIV', $1, $3];"],
+      ["basic < basic", "$$ = ['LT', $1, $3];"],
+      ["basic <= basic", "$$ = ['LTE', $1, $3];"],
+      ["basic > basic", "$$ = ['GT', $1, $3];"],
+      ["basic >= basic", "$$ = ['GTE', $1, $3];"],
+      ["basic == basic", "$$ = ['EQ', $1, $3];"],
+      ["basic ~= basic", "$$ = ['NEQ', $1, $3];"],
+      ["basic && basic", "$$ = ['AND', $1, $3];"],
+      ["basic || basic", "$$ = ['OR', $1, $3];"],
+      ["basic ^^ basic", "$$ = ['XOR', $1, $3];"],
+      ["basic = basic", "$$ = ['ASSIGN', $1, $3];"],
+      ["basic . IDENT", "$$ = ['LOOKUP', $1, ['IDENT', $3]];"],
+      ["basic !", "$$ = ['EXEC', $1, []]"],
+    ],
+    statement: [
+      ["RETURN", "$$ = ['RETURN', ['VALUE', null]];"],
+      ["RETURN expr", "$$ = ['RETURN', $2];"],
+      ["LOOP", "$$ = ['LOOP', ['VALUE', null]];"],
+      ["LOOP expr", "$$ = ['LOOP', $2];"],
       ["IDENT := expr", "$$ = ['DEF', $1, $3];"],
-      ["expr = expr", "$$ = ['ASSIGN', $1, $3];"],
-      ["expr . IDENT", "$$ = ['LOOKUP', $1, ['IDENT', $3]];"],
-      ["IDENT", "$$ = ['IDENT', $1];"],
-      // ["expr ! args", "$$ = ['EXEC', $1, [$3, $4, $5]];"],
-      ["expr !", "$$ = ['EXEC', $1, []];"],
+    ],
+    expr: [
+      ["basic", "$$ = $1"],
+      ["basic ! args", "$$ = ['EXEC', $1, $3]"],
     ],
     args: [
-      ["expr", "$$ = [$1];"],
-      ["args expr", "$$ = $1.concat([$2]);"],
+      ["basic", "$$ = [$1]"],
+      ["args basic", "$$ = $1.concat([$2])"],
     ]
   }
 };
 
 var parser = new Parser(grammar, {type: "lalr"});
 var code = require('fs').readFileSync("sample.jk", "utf8");
-var tree = parser.parse(code);
+var tree = parser.parse(code + "\n");
 var inspect = require('util').inspect;
 console.log(inspect(tree, false, 10, true));
