cb163745e48f4eb77e69f8c6e3ef39fe75d7993b
Tim Caswell <tim@creationix.com>
2013-01-15 15:49:10 -0600

    Fix spacing in parse.js

--- parse.js
+++ parse.js
@@ -1,17 +1,17 @@
 var Parser = require("jison").Parser;
-
 var grammar = require('./grammar');
 var runtime = require('./runtime');
-
-
-
 var filename = process.argv[2] || "sample.jk";
+
 console.log("Parsing: " + filename);
 var code = require('fs').readFileSync(filename, "utf8");
+
 console.log("Creating parser...");
 var parser = new Parser(grammar, {type: "lalr"});
 parser.yy = runtime;
+
 console.log("Parsing file...");
 var tree = parser.parse(code);
+
 console.log("Running program...")
 runtime.call(tree);
