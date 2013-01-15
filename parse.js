fc818d57b3a0703ee1ee10ea26e37cd6183b0557
Tim Caswell <tim@creationix.com>
2013-01-15 15:48:30 -0600

    Properly factor out runtime

--- parse.js
+++ parse.js
@@ -3,11 +3,6 @@ var Parser = require("jison").Parser;
 var grammar = require('./grammar');
 var runtime = require('./runtime');
 
-// Generate all the forms.
-var forms = {};
-grammar.forms.map(function (name) {
-  forms[name] = new runtime.Form(name);
-});
 
 
 var filename = process.argv[2] || "sample.jk";
@@ -15,37 +10,8 @@ console.log("Parsing: " + filename);
 var code = require('fs').readFileSync(filename, "utf8");
 console.log("Creating parser...");
 var parser = new Parser(grammar, {type: "lalr"});
-parser.yy = {
-  P: function (key, value) {
-    return new runtime.Pair(key, value);
-  },
-  L: function (pairs) {
-    var list = new runtime.List();
-    pairs.forEach(function (pair, i) {
-      if (pair instanceof runtime.Pair) {
-        list.insert(pair.value, i);
-        list.alias(pair.key, i);
-      }
-      else {
-        list.insert(pair, i);
-      }
-    });
-    return list;
-  },
-  F: forms,
-  S: function (name) {
-    return new runtime.Symbol(name);
-  },
-  N: function (name) {
-    return new runtime.NativeCode(name);
-  },
-  B: function (str) {
-    return new Buffer(str.substr(1, str.length - 2).split(/\s+/).map(function (byte) {
-      return parseInt(byte, 16);
-    }));
-  }
-};
+parser.yy = runtime;
 console.log("Parsing file...");
 var tree = parser.parse(code);
-console.log("Logging abstract syntax tree...");
-console.log(require('util').inspect(tree, false, 10, true));
+console.log("Running program...")
+runtime.call(tree);
--- runtime.js
+++ runtime.js
@@ -1,10 +1,20 @@
 exports.Pair = Pair
 function Pair(key, value) {
+  if (!(this instanceof Pair)) { return new Pair(key, value); }
   this.key = key;
   this.value = value
 }
 
-exports.Form = Form;
+var forms = exports.forms = {};
+["add", "sub", "mul", "div", "mod", "pow", "and", "or", "xor",
+  "not", "cond", "lte", "lt", "gte", "gt", "neq", "eq", "if", "while",
+  "def", "block", "call", "let", "assign", "fn", "len", "keysof", "typeof",
+  "get", "set", "aget", "aset", "insert", "remove", "slice", "alias", "read",
+  "return", "abort"
+].forEach(function (name) {
+  forms[name] = new Form(name);
+});
+
 function Form(name) {
   this.name = name;
 }
@@ -16,6 +26,7 @@ exports.Symbol = Symbol;
 var symbols = {};
 function Symbol(name) {
   if (symbols[name]) return symbols[name];
+  if (!(this instanceof Symbol)) { return new Symbol(name); }
   symbols[name] = this;
   this.name = name;
 }
@@ -24,34 +35,47 @@ Symbol.prototype.inspect = function () {
 };
 
 exports.List = List;
-function List() {
+function List(pairs) {
+  if (!(this instanceof List)) { return new List(pairs); }
   this.items = [];
   this.aliases = {};
+  if (pairs) {
+    pairs.forEach(function (pair, i) {
+      if (pair instanceof Pair) {
+        this.insert(pair.value, i);
+        this.alias(pair.key, i);
+      }
+      else {
+        this.insert(pair, i);
+      }
+    }, this);
+  }
 }
+List.prototype.checkIndex = function (index) {
+  if (index >> 0 !== index) throw new Error("index must be string or integer");
+  if (index < 0) { index += this.items.length; }
+  return index;
+};
 List.prototype.insert = function (value, index) {
   if (index === null || index === this.items.length) {
     return this.items.push(value);
   }
 };
-List.prototype.set = function (index, value) {
-  if (typeof index === "string") {
-    if (this.aliases[key] === undefined) {
-      index = this.aliases[key];
-    }
-    else {
-      index = this.aliases[key] = this.items.length;
-    }
-  }
-  else {
-    if (index >> 0 !== index) throw new Error("index must be string or integer");
-    if (index < 0) { index += this.items.length; }
-  }
-  this.items[index] = value;
-  return index;
+List.prototype.get = function (index) {
+  index = this.checkIndex(index);
+  var value = this.items[index];
+  if (value === undefined) value = null;
+  return value;
 };
-List.prototype.alias = function (key, index) {
-  this.aliases[key] = index;
+List.prototype.slice = function (start, end) {
+  if (start === null) start = 0;
+  if (end === null) end = this.items.length;
+  start = this.checkIndex(start);
+  end = this.checkIndex(end);
+  // TODO: Should this copy aliases?
+  return new List(this.items.slice(start, end));
 };
+
 var inspect = require('util').inspect;
 List.prototype.inspect = function () {
   var inverse = {};
@@ -74,9 +98,23 @@ exports.NativeCode = NativeCode;
 var nativeCodes = {};
 function NativeCode(name) {
   if (nativeCodes[name]) return nativeCodes[name];
+  if (!(this instanceof NativeCode)) { return new NativeCode(name); }
   nativeCodes[name] = this;
   this.name = name;
 }
 NativeCode.prototype.inspect = function () {
   return "\033[31;1m$" + this.name + "\033[0m";
 };
+
+exports.Buffer = Buffer;
+
+exports.call = call;
+// fn is a List with a @fn form at the root.
+// args is a List with values as args.
+function call(fn, args) {
+  if (fn.get(0) !== forms.fn) throw new Error("Can only execute lists with @fn form");
+  var scope = fn.get(1);
+  var argNames = fn.get(2);
+  var code = fn.slice(3, null);
+  console.log(code);
+}
